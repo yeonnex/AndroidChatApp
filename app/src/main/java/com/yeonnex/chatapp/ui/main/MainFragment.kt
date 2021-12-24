ccdfab3b2da0afa83acd6d7fef69d3b8d813a423
yeonnex <82166132+yeonnex@users.noreply.github.com>
2021-12-24 10:43:11 +0900

    채팅 3초마다 가지고 오기

--- app/src/main/java/com/yeonnex/chatapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/yeonnex/chatapp/ui/main/MainFragment.kt
@@ -3,6 +3,7 @@ package com.yeonnex.chatapp.ui.main
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.os.Handler
+import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,17 +11,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yeonnex.chatapp.AsyncDownThread
+import com.yeonnex.chatapp.ChatItem
 import com.yeonnex.chatapp.ChatRecyclerAdapter
-import com.yeonnex.chatapp.R
 import com.yeonnex.chatapp.databinding.MainFragmentBinding
+import org.json.JSONArray
+import org.json.JSONException
 
 class MainFragment : Fragment() {
-    private var _binding: MainFragment? = null
-    private val binding get() = _binding!!
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter : RecyclerView.Adapter<ChatRecyclerAdapter.ViewHolder>? = null
-    private val items = mutableListOf<ChatItem>()
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = MainFragment()
@@ -28,36 +28,74 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
+    private var layoutManager : RecyclerView.LayoutManager? = null
+    private var adapter : RecyclerView.Adapter<ChatRecyclerAdapter.ViewHolder>? = null
+    private val items = mutableListOf<ChatItem>()
+    private var nickname = "tester"
+    private var lastChatID = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = MainFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-
-        layoutManager = LinearLayoutManager(this)
-        adapter = ChatRecyclerAdapter(context, items) // 프래그먼트 상태이기 때문
+        layoutManager = LinearLayoutManager(context)
+        adapter = ChatRecyclerAdapter(context, items)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = adapter
 
 
-        val url = "http://wik.iptime.org:8080/cmsgs/list/0.json"
         binding.btnSend.setOnClickListener {
-            val thread = AsyncDownThread(url, getChatMsgHandler)
-            thread.start()
-            binding.message.text = "hello"
+
         }
     }
 
     val getChatMsgHandler = Handler {
         val json = it.obj as String
-        binding.message.text = json
+        try {
+            val ja = JSONArray(json)
+            for (i in 0 until ja.length()) {
+                ja.getJSONObject(i).let { jo ->
+                    lastChatID = jo.getInt("id") + 1
+                    val nickname = jo.getString("user_nickname")
+                    val usermsg = jo.getString("user_msg")
+                    if (this.nickname == nickname)
+                        items.add(ChatItem("", usermsg))
+                    else
+                        items.add(ChatItem("$nickname:\n$usermsg"))
+                }
+            }
+            adapter?.notifyDataSetChanged()
+        } catch (e: JSONException) { e.printStackTrace() }
+
         true
     }
+
+    val mHandler: Handler = object : Handler() {
+        override fun handleMessage(msg: Message) {
+            val url = "http://wik.iptime.org:8080/cmsgs/list/$lastChatID.json"
+            AsyncDownThread(url, getChatMsgHandler).start()
+
+            this.sendEmptyMessageDelayed(0, 3000)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mHandler.sendEmptyMessage(0)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mHandler.removeMessages(0)
+    }
+
 
 }
